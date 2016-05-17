1920dff0efd171e70ef4335fd68eb20bb81abcdf
jpark2015 <johnpark@live.com.au>
2016-05-17 14:58:42 +1000

    Update create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -6,6 +6,7 @@ drop table room_type;
 drop table hotel;
 drop table staff;
 drop table customer;
+drop table peak;
 --drop table credit_card;
 
 --staff information
@@ -18,7 +19,6 @@ create table staff(
 	hotel_id int not null,
 	constraint chk_staff_type check (staff_type = 'owner' or staff_type = 'manager'),
 	primary key (id)
-	--foreign key (hotel_id) references hotel(id)
 );
 
 --room information
@@ -80,9 +80,9 @@ create table discount(
 	id int not null generated always as identity,
 	room_type_id int not null,
 	hotel_id int not null,
-	cur_date date not null,
-	start_date date not null,
-	end_date date not null,
+	cur_date varchar(10),
+	start_date varchar(10) not null,
+	end_date varchar(10) not null,
 	rate int not null,
 	constraint chk_rate check (rate > 0 and rate < 100),
 	primary key (id),
@@ -95,7 +95,7 @@ create table discount(
 create table booking(
 	id int not null generated always as identity,
 	customer_id int not null,
-	pin varchar(10),
+	pin varchar(100),
 	primary key (id),
 	foreign key (customer_id) references customer(id)
 );
@@ -103,20 +103,36 @@ create table booking(
 create table detail(
 	id int not null generated always as identity,
 	booking_id int not null,
-	cur_date date not null,
-	start_date date not null,
-	end_date date not null,
+<<<<<<< HEAD
+	start_date varchar(10) not null,
+	end_date varchar(10) not null,
+=======
+	--cur_date date not null,
+	--start_date date not null,
+	--end_date date not null,
+>>>>>>> origin/master
 	hotel_id int not null,
 	room_type_id int not null,
 	extra_bed int not null,
 	constraint chk_extra_bed check (extra_bed = 0 or extra_bed = 1),
-	num_rooms int not null,
+	room_num int,
 	level varchar(10) not null,
-	pin varchar(10),
+	pin varchar(100),
+	price decimal,
 	constraint chk_l check ( level = 'pend' or level = 'confirm' or level = 'assigned'),
 	primary key (id),
 	foreign key (booking_id) references booking(id),
 	foreign key (hotel_id) references hotel(id),
 	foreign key (room_type_id) references room_type(id)
 	--foreign key (pin) references booking(pin)
+);
+create table peak(
+ 	id int not null generated always as identity,
+ 	name varchar(10) not null,
+ 	cur_date varchar(10) not null,
+ 	start_date varchar(10) not null,
+ 	end_date varchar(10) not null,
+ 	rate int not null,
+ 	constraint chk_rate2 check(rate > 0 ),
+ 	primary key (id)
 );
